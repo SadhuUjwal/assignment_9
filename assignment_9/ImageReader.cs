430b7c29042af9fb889f0e6782736e932ec21eca
SadhuUjwal <UjSadhu@gmail.com>
2017-02-24 15:22:58 -0500

    Image Processing

---
+++ assignment_9/ImageReader.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+using PCALib;
+
+namespace assignment_9
+{
+    //The Image reader reads all the images from a directory and converts each of them
+    //to a vector. The vectos are the columnswise concaternated. The class labels are
+    //determined by the filename, e.g. S10_3.jpg is the class 10.
+    //The injection of the transformation method is handeld by a strategy pattern
+    //with the abstract base class ImageFeatureExtractor.
+    public class ImageReader
+    {
+        private String path;
+        private ImageFeatureExtractor featureExtractor;
+
+        //Reads the images in the directory path into the matrix X by transforming each image
+        //into a vector. Each column corresponds to a image.
+        //In Y are the class labels. Y is a vector. Each element corresponds to the class. The 
+        //class is determined by the filename of the image. S10_3.jpg is class 10, etc.
+        //This image reader reads only the gray values
+        public ImageReader(String path, ImageFeatureExtractor featureExtractor)
+        {
+            this.path = path;
+            this.featureExtractor = featureExtractor;
+        }
+
+        public void setFeatureExtractor(ImageFeatureExtractor featureExtractor)
+        {
+            this.featureExtractor = featureExtractor;
+        }
+
+        public void transform(out IMatrix X, out IMatrix Y)
+        {
+            //Read the images and their classes into lists
+            List<IMatrix> imageVectors = new List<IMatrix>();
+            List<double> imageClasses = new List<double>();
+            foreach (var f in Directory.GetFiles(path, "*.jpg"))
+            {
+                //Console.WriteLine(System.IO.Path.GetFileName(f)); // file name
+                Bitmap img = new Bitmap(Image.FromFile(f));
+                imageVectors.Add(featureExtractor.transform(img));
+                imageClasses.Add(Path2Class(f));
+            }
+            //Store the results in the matrices X and Y
+            var n = imageVectors.Count;
+
+            var d = imageVectors[0].Rows;
+            X = new Matrix(d, n);
+            Y = new Matrix(1, n);
+
+            for (int i = 0; i < n; i++)
+            {
+                Y[0, i] = imageClasses[i];
+                for (int j = 0; j < d; j++)
+                {
+                    X[j, i] = imageVectors[i][j, 0];
+                }
+            }
+            Console.WriteLine("Loaded " + n + " images.");
+        }
+
+        //Coverts a path like S10_3.jpg into the class 10
+        protected double Path2Class(String path)
+        {
+            double res = 0;
+            var regex = new Regex(@"(\d+)(?=_)");
+            var filename = System.IO.Path.GetFileName(path);
+            var match = regex.Match(filename);
+            if (match.Success)
+            {
+                Double.TryParse(match.Value, out res);
+            }
+            return res;
+        }
+
+    }
+
+
+
+    //Base class for extractors that transform images to vectors.
+    public abstract class ImageFeatureExtractor
+    {
+        abstract public IMatrix transform(Bitmap x);
+    }
+
+    //Converts Images to vectors by calculating the LBPs
+    public class LBPFeatureExtractor : ImageFeatureExtractor
+    {
+
+        int radius = 1; //radius for LBP
+
+        public LBPFeatureExtractor(int LBPradius)
+        {
+            radius = LBPradius;
+        }
+
+        /// <summary>
+        /// Computes LBP matrix of the image with radius r
+        /// </summary>
+        /// <param name="srcBmp">source image for which LBP needs to be computed</param>
+        /// <param name="r">radius of LBP</param>
+        /// <param name="max">max value of the LBP matrix will be returned</param>
+        /// <returns>Computed LBP matrix</returns>
+        public static double[] LBPMatrix(Bitmap srcBmp, int r, out double max)
+        {
+            max = 0.0;
+            //1. Extract rows and columns from srcImage. Note Source image is Gray scale Converted Image
+            int NumRow = srcBmp.Height;
+            int numCol = srcBmp.Width;
+            List<double> histMat = new List<double>();
+            //2. Loop through Pixels
+            for (int i = 0; i < NumRow; i++)
+                for (int j = 0; j < numCol; j++)
+                    //define boundary condition, other wise say if you are looking at pixel (0,0), 
+                    //it does not have any suitable neighbors
+                    if ((i > r) && (j > r) && (i < (NumRow - r)) && (j < (numCol - r)))
+                    {
+                        // we want to store binary values in a List
+                        List<int> vals = new List<int>();
+                        try
+                        {
+                            for (int i1 = i - r; i1 < (i + r); i1++)
+                                for (int j1 = j - r; j1 < (j + r); j1++)
+                                {
+                                    int acPixel = srcBmp.GetPixel(j, i).R;
+                                    int nbrPixel = srcBmp.GetPixel(j1, i1).R;
+                                    // 3. This is the main Logic of LBP
+                                    if (nbrPixel > acPixel)
+                                        vals.Add(1);
+                                    else
+                                        vals.Add(0);
+                                }
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                        //4. Once we have a list of 1's and 0's , convert the list to decimal
+                        // Also for normalization purpose calculate Max value
+                        double dec = ToBinary(vals);
+                        histMat.Add(dec);
+                        if (dec > max)
+                            max = dec;
+                    }
+            return histMat.ToArray();
+        }
+
+        /// <summary>
+        /// Convertes list of binary digits to a decimal value
+        /// </summary>
+        /// <param name="binary">list of binary digits</param>
+        /// <returns>decimal value of the list</returns>
+        private static double ToBinary(List<int> binary)
+        {
+            double d = 0;
+
+            for (int i = 0; i < binary.Count; i++)
+                d += binary[i] * Math.Pow(2, i);
+            return d;
+        }
+
+        /// <summary>
+        /// computes normalized array of a double array 
+        /// </summary>
+        /// <param name="Mat">double array which is to be normalized</param>
+        /// <param name="max">max value of that array</param>
+        /// <returns>normalized array values</returns>
+        private static double[] NormalizeLbpMatrix(double[] Mat, double max)
+        {
+            for (int i = 0; i < Mat.Length; i++)
+                Mat[i] = Mat[i] / max;
+            return Mat;
+        }
+
+        /// <summary>
+        /// Provides normalized LBP array of an image
+        /// </summary>
+        /// <param name="src">source image for which LBP need to e calculated</param>
+        /// <param name="r">radius of LBP</param>
+        /// <returns></returns>
+        private static double[] NormalizedLBP(Bitmap src, int r)
+        {
+            double max = 0.0;
+
+            var testGray = GrayConversion(src);
+            double[] srcMat = NormalizeLbpMatrix(LBPMatrix(testGray, r, out max), max);
+            return srcMat;
+        }
+
+        /// <summary>
+        /// Converts the provided image to gray image
+        /// </summary>
+        /// <param name="srcBmp">image which needs to be converted as gray image</param>
+        /// <returns>gray image</returns>
+        public static Bitmap GrayConversion(Bitmap srcBmp)
+        {
+            int NumRow = srcBmp.Height;
+            int numCol = srcBmp.Width;
+            Bitmap gray = new Bitmap(srcBmp.Width, srcBmp.Height);
+
+            for (int i = 0; i < NumRow; i++)
+                for (int j = 0; j < numCol; j++)
+                {
+                    // Extract the color of a pixel
+                    Color c = srcBmp.GetPixel(j, i);
+                    // extract the red,green, blue components from the color.
+                    int rd = c.R; int gr = c.G; int bl = c.B;
+                    double d1 = 0.2989 * (double)rd + 0.5870 * (double)gr + 0.1140 * (double)bl;
+                    int c1 = (int)Math.Round(d1);
+                    Color c2 = Color.FromArgb(c1, c1, c1);
+                    gray.SetPixel(j, i, c2);
+                }
+            return gray;
+        }
+
+        //Extract from a single image x the LBP.
+        public override IMatrix transform(Bitmap x)
+        {
+            double[] lbp = NormalizedLBP(x, radius);
+            IMatrix res = new Matrix(lbp.Length, 1);
+            for (int i = 0; i < lbp.Length; i++)
+            {
+                res[i, 0] = lbp[i];
+            }
+            return res;
+        }
+    }
+
+    //Converts a bitmap into a column vector by concaternating all the gray values.
+    public class ConcatGrayValueExtracor : ImageFeatureExtractor
+    {
+        public override IMatrix transform(Bitmap x)
+        {
+            int NumRow = x.Height;
+            int numCol = x.Width;
+            IMatrix res = new Matrix(NumRow * numCol, 1);
+            for (int i = 0; i < NumRow; i++)
+                for (int j = 0; j < numCol; j++)
+                {
+                    // Extract the color of a pixel
+                    Color c = x.GetPixel(j, i);
+                    // extract the red,green, blue components from the color.
+                    int rd = c.R; int gr = c.G; int bl = c.B;
+                    double d1 = 0.2989 * (double)rd + 0.5870 * (double)gr + 0.1140 * (double)bl;
+                    int c1 = (int)Math.Round(d1);
+                    res[j + i * numCol, 0] = c1;
+                }
+            return res;
+        }
+    }
+}
---
+++ assignment_9/Program.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+using System.Text.RegularExpressions;
+
+using PCALib;
+
+namespace assignment_9
+{
+    class MathHelper
+    {
+        //Creates a matrix that contains only ones.
+        public static IMatrix ones(int nrow, int ncol)
+        {
+            Matrix ones = new Matrix(nrow, ncol);
+            for (int i = 0; i < nrow; i++)
+            {
+                for (int j = 0; j < ncol; j++)
+                {
+                    ones[i, j] = 1;
+                }
+            }
+            return ones;
+        }
+
+        public static int[] sortIndex(double[] arr)
+        {
+            int[] index = Enumerable.Range(0, arr.Length).ToArray();
+            Array.Sort(arr, index);
+            return (index);
+        }
+    }
+
+    class KNN
+    {
+        private int kN; //number of nearest neighbours to use
+        private IMatrix Xfit; //data features stored. from These points are the nearest neighbours chosen.
+        private IMatrix Yfit; //data class labels. from These points are the nearest neighbours chosen.
+
+
+        //X: d x n matrix. 	each column contains a d dimensional instance
+        //Y: 1 x m matrix. each element is the corresponding class label.
+        //k: integer 		number of nearest neighbours to use.
+        public void fit(IMatrix X, IMatrix Y, int k = 2)
+        {
+            kN = k;
+            Xfit = X.Clone();
+            Yfit = Y.Clone();
+        }
+
+
+        //Transform new data. The KNN must be fitted with fit before using this.
+        //x: d x 1 matrix. The features of a single instance 
+        public double transformSingleInstance(IMatrix x)
+        {
+            //Compute the squared euclidean distance to all points in X.
+            //To this end, subtract x from each intance and save the results in diff.
+            var n = Xfit.Columns;
+            var d = Xfit.Rows;
+            var diff = Xfit.Subtraction(x.Multiply(MathHelper.ones(1, n)));
+            //Console.WriteLine (diff.ToString());
+            //Compute the euclidean norm for each column
+            double[] distance = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                var column = diff.Submatrix(0, d - 1, i, i); // the ith column
+                var s = column.Transpose().Multiply(column);
+                distance[i] = s.Trace;
+            }
+            //Get the first kN indices which columns are the nearest to x.
+            int[] indx = MathHelper.sortIndex(distance).Take(kN).ToArray();
+
+            //Get the labels of the nearest intances.
+            IMatrix nearestLabels = Yfit.Submatrix(0, 0, indx);
+            //Count the number of occurences of the different labels
+            Dictionary<double, double> frequency =
+                new Dictionary<double, double>();
+            for (int i = 0; i < nearestLabels.Columns; i++)
+            {
+                var label = nearestLabels[0, i];
+                if (frequency.ContainsKey(label))
+                {
+                    frequency[label] += 1.0;
+                }
+                else {
+                    frequency.Add(label, 1.0);
+                }
+            }
+            //Order it by the number of occurences and return the label that occurs most often
+            return frequency.OrderByDescending(i => i.Value).First().Key;
+        }
+
+        //Transform new data. The KNN must be fitted with fit before using this.
+        //x: d x n matrix. The features of a single instance 
+        public IMatrix transform(IMatrix X)
+        {
+            var n = X.Columns;
+            var d = X.Rows;
+            IMatrix y = new Matrix(1, n);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("\r{0}% ", 100 * ((double)i) / n);
+                y[0, i] = transformSingleInstance(X.Submatrix(0, d - 1, i, i));
+            }
+            Console.WriteLine(" done.");
+            return (y);
+        }
+    }
+
+    class PCA
+    {
+        public IMatrix mean;
+        public IMatrix principleComponents;
+
+        //Fit the PCA to the data in X. Must be done before using it with transform.
+        //X: d x n matrix. 	each column contains a d dimensional instance
+        //r: integer. 		number of principle components
+        public void fit(IMatrix X, int r = 2)
+        {
+            int d = X.Rows;
+            int n = X.Columns;
+            IEigenvalueDecomposition decomp;
+            r = Math.Min(r, d);
+            //Multiply by a vector of 1s and devide by n to get the mean
+            mean = X.Multiply(MathHelper.ones(n, 1)).Multiply(1.0 / n);
+            //Console.WriteLine (mean.ToString());
+
+            //Subtract the mean form every column. Now the instances in Xmean have zero mean.
+            IMatrix XMean = X.Subtraction(mean.Multiply(MathHelper.ones(1, n)));
+            //Console.WriteLine (XMean.ToString());
+
+
+            //Now we want to compute the eigenvectors/values of the covariance matrix
+            //XMean.Multiply (XMean.Transpose()). This can be done as below.
+            //However, when we have many features, then this is slow. The second
+            //appraoch is faster and was suggested.
+
+            //Create the (scaled) convariance matrix
+            //IMatrix Cov = XMean.Multiply (XMean.Transpose());
+            //Get the eigenvalues/vectors. The eigenvalues are sorted in ascending order
+            //decomp = Cov.GetEigenvalueDecomposition();
+            //double[] evalues = decomp.RealEigenvalues;
+            //Get the eigenvectors with the largest eigenvectors, i.e. the last r eigenvectors
+            //principleComponents = decomp.EigenvectorMatrix.Submatrix (0,d-1,d-1-r+1,d-1);
+
+            IMatrix M = XMean.Transpose().Multiply(XMean);
+            //Get the eigenvalues/vectors. The eigenvalues are sorted in ascending order
+            decomp = M.GetEigenvalueDecomposition();
+            double[] evalues = decomp.RealEigenvalues;
+            //Get the eigenvectors with the largest eigenvectors, i.e. the last r eigenvectors
+            // and multiply them with Xmean (to get the eigenvectors of the covariance matrix
+            principleComponents = XMean.Multiply(decomp.EigenvectorMatrix.Submatrix(0, n - 1, n - 1 - r + 1, n - 1));
+        }
+
+        //Transform new data. The PCA must be fitted with fit before using this.
+        //X: d x n matrix. each column contains a d dimensional instance 
+        public IMatrix transform(IMatrix X)
+        {
+            int d = X.Rows;
+            int n = X.Columns;
+            //Subtract the mean form every column.
+            IMatrix XMean = X.Subtraction(mean.Multiply(MathHelper.ones(1, n)));
+            return (principleComponents.Transpose().Multiply(XMean));
+        }
+
+    }
+
+    class MainClass
+    {
+        //Compute accuray by comparing the predicted labels Yhat and the ground truth Y.
+        private static double computeAccuracy(IMatrix Yhat, IMatrix Y)
+        {
+            var n = Yhat.Columns;
+            int truepositive = 0;
+            for (int i = 0; i < n; i++)
+            {
+                truepositive += Convert.ToInt32(Yhat[0, i] == Y[0, i]);
+            }
+            return (((double)truepositive) / n);
+        }
+
+
+        //Function to test the PCA and KNN.
+        public static void TestPCAKNN()
+        {
+            PCA pca = new PCA();
+            KNN knn = new KNN();
+            Matrix X1 = new Matrix(3, 5);
+            X1[0, 0] = 2.0;
+            X1[0, 1] = 1.0;
+            X1[0, 2] = 2.0;
+            X1[0, 3] = 2.0;
+            X1[0, 4] = 1.0;
+            X1[1, 0] = 1.0;
+            X1[1, 1] = 4.0;
+            X1[1, 2] = 0.0;
+            X1[1, 3] = 4.0;
+            X1[1, 4] = 1.0;
+            X1[2, 0] = 2.0;
+            X1[2, 1] = 0.0;
+            X1[2, 2] = 8.0;
+            X1[2, 3] = 1.0;
+            X1[2, 4] = 1.0;
+            Matrix X2 = new Matrix(3, 3);
+            X2[0, 0] = 2.0;
+            X2[0, 1] = 1.2;
+            X2[0, 2] = 0.0;
+            X2[1, 0] = 1.0;
+            X2[1, 1] = 3.0;
+            X2[1, 2] = 0.0;
+            X2[2, 0] = 2.0;
+            X2[2, 1] = 0.0;
+            X2[2, 2] = 0.0;
+            Matrix Y1 = new Matrix(1, 5);
+            Y1[0, 0] = 1;
+            Y1[0, 1] = 2;
+            Y1[0, 2] = 3;
+            Y1[0, 3] = 1;
+            Y1[0, 4] = 5;
+
+            Console.WriteLine("X1 = ");
+            Console.WriteLine(X1.ToString());
+            Console.WriteLine("X2 = ");
+            Console.WriteLine(X2.ToString());
+
+            pca.fit(X1);
+
+            var Z1 = pca.transform(X1);
+            var Z2 = pca.transform(X2);
+
+            Console.WriteLine("Z1 = ");
+            Console.WriteLine(Z1.ToString());
+            Console.WriteLine("Z2 = ");
+            Console.WriteLine(Z2.ToString());
+
+            knn.fit(X1, Y1, 3);
+            var res = knn.transformSingleInstance(X2.Submatrix(0, 2, 0, 0));
+            Console.WriteLine(res.ToString());
+
+            var res2 = knn.transform(X2);
+            Console.WriteLine(res2.ToString());
+        }
+
+        public static void TestReader()
+        {
+            ImageReader ireader;
+            IMatrix X = new Matrix(0, 0);
+            IMatrix Y = new Matrix(0, 0);
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK)
+                return;
+            ireader = new ImageReader(fbd.SelectedPath, new ConcatGrayValueExtracor());
+            ireader.transform(out X, out Y);
+            Console.WriteLine(X);
+            Console.WriteLine(Y);
+        }
+
+        public static void run()
+        {
+            //Open the training data.
+            ImageReader traningImagereader;
+            ImageReader testImagereader;
+            IMatrix X = new Matrix(0, 0);
+            IMatrix Y = new Matrix(0, 0);
+            PCA pca = new PCA(); //The principle component model for the concaternated gray values
+            KNN knnPca = new KNN(); //The KNN model trained on the data after the PCA
+            KNN knnLBP = new KNN(); //The KNN model trained on the LBP features.
+
+            int kNNPCA = 3; //Number of nearest neighbours for PCA
+            int kNNLBP = 6; //Number of nearest neighbours for LBP
+            int nPC = 10; //Number of principle components
+            int LBPRadius = 1; //Radius for LBP
+
+            //***************TRAINING***************
+            Console.WriteLine("Please select the directory with the training images.");
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK)
+                return;
+            traningImagereader = new ImageReader(fbd.SelectedPath, new ConcatGrayValueExtracor());
+
+            //Train 1.Model: raw gray value pictue + pca + knn
+            Console.WriteLine("Train PCA + KNN...");
+            traningImagereader.transform(out X, out Y); //Read features into X and labels into Y
+            pca.fit(X, nPC); //Train the PCA
+            var Z = pca.transform(X); //Transform with the PCA
+            knnPca.fit(Z, Y, kNNPCA); //Train the KNN on the transformed values
+            Console.WriteLine("");
+
+            //Train 2.Model: LBP featurs + knn
+            Console.WriteLine("Train LBP + KNN...");
+            traningImagereader.setFeatureExtractor(new LBPFeatureExtractor(LBPRadius)); //Set feature extractor to LBP
+            traningImagereader.transform(out X, out Y); //Read features into X and labels into Y
+            knnLBP.fit(X, Y, kNNLBP); //Train the KNN on the LBP values
+            Console.WriteLine("");
+
+
+            //***************TESTING***************
+            Console.WriteLine("Please select the directory with the test images.");
+            fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() != DialogResult.OK)
+                return;
+            testImagereader = new ImageReader(fbd.SelectedPath, new ConcatGrayValueExtracor());
+
+
+            //Evaluate 1. Model: PCA + KNN
+            Console.WriteLine("Test PCA + KNN...");
+            testImagereader.transform(out X, out Y); //Read features into X and labels into Y
+            Z = pca.transform(X);
+            var YhatPCA = knnPca.transform(Z);
+            var accuracy = computeAccuracy(YhatPCA, Y);
+            Console.WriteLine("Accuray of Raw Picture + PCA + KNN: " + accuracy);
+            Console.WriteLine("");
+
+            //Evaluate 2.Model: LBP + KNN
+            Console.WriteLine("Test LBP + KNN...");
+            testImagereader.setFeatureExtractor(new LBPFeatureExtractor(LBPRadius)); //Set feature extractor to LBP
+            testImagereader.transform(out X, out Y); //Read features into X and labels into Y
+            var YhatLBP = knnLBP.transform(X); //Predict with the KNN from the LBP values
+            accuracy = computeAccuracy(YhatLBP, Y);
+            Console.WriteLine("Accuray of LBP + KNN: " + accuracy);
+            Console.WriteLine("");
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            run();
+            Console.ReadLine(); 
+        }
+    }
+}
